efb6ea299a0c353a79b0b3dd5177e053f620d9bb
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-11 15:22:55 -0700

    I have added my first test

--- tests/items.test.js
+++ tests/items.test.js
@@ -3,6 +3,16 @@ const app = require('../lib/app');
 
 describe('route tests', () => {
   it('can create an item with post', () => {
-  
+    return request(app)
+      .post('/api/v1/farts')
+      .send({ type: 'wet-fart', description: 'wet sound that can lead to seepage', deadly: true })
+      .then(res => {
+        expect(res.body).toEqual({ 
+          type: 'wet-fart', 
+          description: 'wet sound that can lead to seepage', 
+          deadly: true 
+        });
+      });
+
   });
 });
